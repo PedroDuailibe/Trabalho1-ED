166335e324aa084b1c234f55114e91581dc5715c
PedroDuailibe <pedrohduailibe@gmail.com>
2026-03-27 15:48:47 -0300

    questao 1 corrigida

--- ReservationRequest.cpp
+++ ReservationRequest.cpp
@@ -1,33 +1,26 @@
-#include<iostream>
+#include <iostream>
+#include "ReservationRequest.hpp"
 
-class ReservationRequest {
-
-private:
-
-    std::string course_name;
-    std::string weekday;
-    int start_hour;
-    int end_hour;
-    int student_count;
-
-public:
-
-    ReservationRequest(std::string course_name, std::string weekday, int start_hour, int end_hour, int student_count)
-      : course_name(course_name)
-      , weekday(weekday)
-      , start_hour(start_hour)
-      , end_hour(end_hour)
-      , student_count(student_count)
+ReservationRequest::ReservationRequest(std::string course_name, std::string weekday, int start_hour, int end_hour, int student_count)
+  : course_name(course_name)
+  , weekday(weekday)
+  , start_hour(start_hour)
+  , end_hour(end_hour)
+  , student_count(student_count)
+{
+    if (start_hour < 7 || end_hour > 21 || start_hour >= end_hour)
     {
+        std::cerr << "Horário inválido." << "\n"; 
     }
 
-    ~ReservationRequest()
-    {
-    }
+}
 
-    int getStartHour();
-    int getEndHour();
-    std::string getCourseName();
-    std::string getWeekday();
-    int getStudentCount();
-};
+ReservationRequest::~ReservationRequest()
+{
+}
+
+int ReservationRequest::getStartHour() { return start_hour; }
+int ReservationRequest::getEndHour() { return end_hour; }
+std::string ReservationRequest::getCourseName() { return course_name; }
+std::string ReservationRequest::getWeekday() { return weekday; }
+int ReservationRequest::getStudentCount() { return student_count; }
--- ReservationSystem.cpp
+++ ReservationSystem.cpp
@@ -1,37 +1,18 @@
 #include <iostream>
-#include "ReservationRequest.hpp"
+#include "ReservationSystem.hpp"
 
-class ReservationSystem {
-
-private:
-    int room_count;
-    int* room_capacities;
-
-    // Estruturas internas escolhidas pelos alunos
-    // para armazenar e gerenciar as reservas, os horários, ...
-
-public:
-
-    ReservationSystem(int room_count, int* room_capacities)
-      : room_count(room_count)
-      , room_capacities(new int[room_count])
+ReservationSystem::ReservationSystem(int room_count, int* room_capacities)
+  : room_count(room_count)
+  , room_capacities(new int[room_count])
+{
+    for (int i = 0; i < room_count; i++)
     {
-        for (int i = 0; i < room_count; i++)
-        {
-            this->room_capacities[i] = room_capacities[i];
-        }
+        this->room_capacities[i] = room_capacities[i];
     }
-    
-    ~ReservationSystem()
-    {
-        delete[] room_capacities;
-    }
+}
 
-    bool reserve(ReservationRequest request);
-    bool cancel(std::string course_name);
+ReservationSystem::~ReservationSystem()
+{
+    delete [] this->room_capacities;
+}
 
-    void printSchedule();
-
-    // Outros métodos utilitários necessários
-    // para auxiliar nas funções requisitadas
-};
